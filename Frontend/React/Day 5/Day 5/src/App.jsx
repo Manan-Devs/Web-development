3ca8baa9d236ffa07738d374565907257e89b039
Manan Bhatt <mananbhatt1010@gmail.com>
2025-12-06 23:53:07 +0530

    learning useEffect hooks code

---
+++ Frontend/React/Day 5/Day 5/src/App.jsx
@@ -0,0 +1,47 @@
+import {useEffect, useState} from "react";
+
+// user
+
+function App() {
+     
+    const [users , setUsers] = useState([]);
+
+    // const [name,setName] = useState("");
+
+    const [count,setCount] = useState(30);
+
+        useEffect(()=> {
+           async function GithubProfile() {
+
+       const response = await fetch(`https://api.github.com/users?per_page=${count}`);
+       const data = await response.json(); 
+       setUsers(data);
+       console.log("Hello");
+  }
+        GithubProfile();
+        },[count]);
+
+        //  function handleChange(e) {
+        //      setName(e.target.value.toUpperCase());
+        //  }
+
+    return ( 
+        <>
+          <h1>Github User</h1> 
+          <input type="number" value={count} onChange={(e) => setCount(e.target.value) }></input>
+          <div style = {{display:"flex", justifyContent:"center", alignContent:"center", flexWrap:"wrap",gap:"20px"}}>
+             {
+                users.map(users=> (
+                    <img src={users.avatar_url} height={"100px"} width={"100px"} key={users.login} />
+                )) 
+             } 
+          </div>
+        </>
+    )
+}
+
+
+
+
+
+export default App;
