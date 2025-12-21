175ada74a55534eebcd6e52c9bb567a3011ed667
Manan Bhatt <mananbhatt1010@gmail.com>
2025-12-21 22:15:02 +0530

    updating UI by building new section

--- Frontend/React/Projects/Personal Dashboard/src/components/DashboardGrid.jsx
+++ Frontend/React/Projects/Personal Dashboard/src/components/DashboardGrid.jsx
@@ -1,12 +1,14 @@
 import TodoWidget from "./TodoWidget";
 import TimerWidget from "./TimerWidget";
 import WeatherWidget from "./WeatherWidget";
+import NotesWidget from "./NotesWidget";
 function DashboardGrid() {
   return (
     <div className="dashboard-grid">
         <TodoWidget />
         <TimerWidget />
         <WeatherWidget/>
+        <NotesWidget/>
     </div>
   );
 }
---
+++ Frontend/React/Projects/Personal Dashboard/src/components/NotesWidget.jsx
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+function NotesWidget() {
+     return ( 
+         <div className="Notes__Container">
+          <h1>Quick Notes</h1>
+          <textarea placeholder="Enter your text here..." name="notes" id="notes">Enter your text here</textarea>
+        </div>
+     )
+      
+      
+}
+
+
+export default NotesWidget;
